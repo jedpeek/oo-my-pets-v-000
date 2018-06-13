742b6a9f08a62df68580c76a87d7faf1cdca83ab
Jed Peek <jedpeek@yahoo.com>
2018-06-13 16:10:27 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -48,7 +48,10 @@ class Owner
   end
 
   def list_pets
-    
+  fishes = @pets[:fishes].length
+  dogs = @pets[:dogs].length
+  cats = @pets[:cats].length
+  "I have #{fishes} fish, #{dogs} dog(s), and #{cats} cat(s) "
 
 
 end
